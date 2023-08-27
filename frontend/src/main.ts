79f90cd15e8d523365787d69e27af6ac9946d4bc
laurabiro <laurabiro90@gmail.com>
2023-08-28 00:29:12 +0200

    to show

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
-import "./style.css";
-import axios from "axios";
-import { z } from "zod";
+import "./style.css"
+import axios from "axios"
+import { z } from "zod"
 
 const PizzaSchema = z.object ({
   id: z.number(),
@@ -29,6 +29,14 @@ let order: Order | null = null
 let isSending = false
 
 // mutation
+
+const selectCard = document.getElementById("selected") as HTMLDivElement
+const orderCard = document.getElementById("order") as HTMLDivElement
+selectCard.style.display = "none"
+orderCard.style.display = "none"
+
+const date = new Date().toUTCString();
+
 const getPizzas = async () => {
   isLoading = true
 
@@ -42,12 +50,52 @@ const getPizzas = async () => {
     pizzas = result.data
 }
 
+const postPizzas = async () => {
+  isSending = true
+  
+    const nameInput = document.getElementById("name") as HTMLInputElement;
+    const zipInput = document.getElementById("zip") as HTMLInputElement;
+
+    const name = nameInput.value;
+    const zipCode = zipInput.value;
+
+    const orderField = {
+      "ordered pizzas":getOrderDetails(),
+      "name": name,
+      "zip code": zipCode,
+      "date":date,
+    }
+  
+  try {
+    const config = {
+      method: "POST",
+      body: JSON.stringify(orderField),
+    }
+    console.dir(config)
+    const response = await axios.post('http://localhost:3333/api/order', orderField)
+    console.log('Data sent successfully:', response.data)
+  } catch (error) {
+    console.error('Error sending data:', error)
+  }
+}
+
 const selectPizza = (id: number) => {
   selectedPizza = pizzas.find(pizza => pizza.id === id) || null
 }
 
 const updateAmount = (num: number) => {
   amount = num
+}
+
+const deleteItem = (id:number) => {
+  if (order) { order = {
+    name: order.name,
+    zipCode: order.zipCode,
+    items: [
+      ...order.items.filter(item => item.id !== id)
+    ]
+  } 
+}
 }
 
 const updateOrderWithItem = () => {
@@ -67,11 +115,32 @@ const updateOrderWithItem = () => {
   }
 }
 
+const getOrderDetails = () => {
+  const listedPizzas = document.getElementsByClassName("ordering-pizzas")
+  let i = 0
+  let result:string[] = []
+  while(listedPizzas[i] !== undefined){
+    result = [...result, listedPizzas[i].innerHTML]
+    i++
+  }
+  return result
+}
+
+const deletePost = () => {
+  if (order) { order = {
+    name: "",
+    zipCode: "",
+    items: [
+    ]
+  } 
+}
+}
+
 
 // render
 const renderList = (pizzas: Pizza[]) => {
   const container = document.getElementById("list")!
-
+  
   for (const pizza of pizzas) {
     const pizzaParagraph = document.createElement("p")
     pizzaParagraph.innerText = pizza.name
@@ -85,8 +154,8 @@ const renderSelected = (pizza: Pizza) => {
   const content = `
     <div>
       <h1>${pizza.name}</h1>
-      <p class="bg-red-600">${pizza.toppings}</p>
-      <img src="${pizza.url}" />
+      <p id="" class="bg-red-600">${pizza.toppings}</p>
+      <img id="img" src="${pizza.url}" />
       <input type="number" id="amount" />
       <button id="add">Add to order</button>
     </div>
@@ -101,19 +170,32 @@ const renderOrder = (order: Order) => {
     <div>
       <h1>Your order</h1>
       ${order.items.map(item => `
-        <p class="bg-red-500">${item.amount} x ${pizzas.find(pizza => pizza.id === item.id)!.name}</p>
-      `)}
-      <input placeholder="Name">
-      <input placeholder="Zip code">
-      <button>Send order</button>
+        <div class="orderline">
+        <p class="ordering-pizzas bg-red-500 ">${item.amount} x ${pizzas.find(pizza => pizza.id === item.id)!.name}</p>
+        <button id="x-${item.id}">X</button>
+        </div>
+      `).join('')}
+      <input placeholder="Name" id="name" />
+      <input placeholder="Zip code" id="zip" />
+      <button id="send" >Send order</button>
     </div>
   `
 
   document.getElementById("order")!.innerHTML = content
+
+  for (let i = 0; i < order.items.length; i++) {
+    let item = order.items[i]
+      document.getElementById(`x-${item.id}`)!.addEventListener("click", deleteListener)
+  }
+  document.getElementById("send")!.addEventListener("click", postListener)
+  document.getElementById("send")!.addEventListener("click", deletePostListener)
 }
 
+
+
+
 // eventListeners
-const init = async () => {
+const init = async () => {
   await getPizzas()
   if (pizzas.length)
     renderList(pizzas)
@@ -123,6 +205,8 @@ const selectListener = (event: Event) => {
   selectPizza(+(event.target as HTMLParagraphElement).id)
   if (selectedPizza)
     renderSelected(selectedPizza)
+
+  selectCard.style.display = "block"
 }
 
 const changeListener = (event: Event) => {
@@ -133,7 +217,33 @@ const addListener = () => {
   updateOrderWithItem()
   if (order)
     renderOrder(order)
+
+  orderCard.style.display = "block"
 }
+
+const deleteListener = (event: Event) => {
+  deleteItem(+(event.target as HTMLButtonElement).id.split("-")[1]);
+  if (order) {
+    renderOrder(order)
+  }
+}
+
+const postListener = () => {
+  postPizzas()
+  updateOrderWithItem()
+  if(order){
+    renderOrder(order)
+  }
+}
+
+const deletePostListener = () => {
+  deletePost()
+  orderCard.style.display = "none"
+  selectCard.style.display = "none"
+}
+
+
+
 
 
 init()
